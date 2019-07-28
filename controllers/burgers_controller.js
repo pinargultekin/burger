45d1cec52fc59d9c42a47e6b3a4f0e6673f2e70e
29bucuk <pinargultekin@yahoo.com>
2019-07-28 13:29:35 -0400

    delete router added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,59 @@
-var express = require ("express");
+var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger");
 
 //Get router
-router.get("/", function(req, res){
-    burger.all(function(data){
+router.get("/", function (req, res) {
+    burger.all(function (data) {
         var hbsObject = {
             burgers: data
         };
-console.log(hbsObject);
-res.render("index", hbsObject);
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
 });
 
 //Post router
-router.post("/api/burgers", function(req, res){
+router.post("/api/burgers", function (req, res) {
     burger.create([
         "burger_name", "devoured"
     ], [
-        req.body.burger_name, req.body.devoured
-    ], function(result) {
-        res.json ({ id: result.insertId });
-    });
+            req.body.burger_name, req.body.devoured
+        ], function (result) {
+            res.json({ id: result.insertId });
+        });
 });
 
 //Put Router
-router.put("/api/burgers/:id", function(req, res){
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-    console.log( "condition: " + conditon);
+    console.log("condition: " + conditon);
 
-    burger.update({devoured: req.body.devoured}, condition, function (result){
-       
-        if(result.changedRows ==0){
+    burger.update({ devoured: req.body.devoured }, condition, function (result) {
+
+        if (result.changedRows == 0) {
             return res.status(404).end();
-        
-        }else{
+
+        } else {
             res.status(200).end();
 
         }
     });
 });
+
+//Delete Router
+router.delete("api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition: " + condition);
+    burger.delete(condition, function (result) {
+        if (result.affectedRows === 0) {
+            return res.status(404).end();
+
+        } else {
+            res.status(200).end();
+        };
+    });
+});
+
+module.exports = router;
