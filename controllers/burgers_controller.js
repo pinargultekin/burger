b5a60ff036e9a5b1beb57b4f520ccd1f23d69fdb
29bucuk <pinargultekin@yahoo.com>
2019-07-28 13:18:10 -0400

    get router added

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,16 @@
+var express = require ("express");
+var router = express.Router();
+
+var burger = require("../models/burger");
+
+//Get router
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+console.log(hbsObject);
+res.render("index", hbsObject);
+    });
+});
+
